0ffeba6feb1de0f97a21edf949addd8d93c31311
Mauro <mauropaccioretti@hotmail.com>
2022-05-04 20:50:04 -0300

    first commit

---
+++ src/Formulario.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import "./Formulario.css";
+
+const Formulario = () => {
+  return (
+    <Container id="main-container" className="d-grid h-100">
+      <Form id="sing-in-form" className="text-center w-100">
+        <img
+          className="mb-4 bootstrap-logo"
+          src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg"
+          alt="bootstrap 5"
+        />
+        <h1 className="fs-3 fw-normal">Complete sus datos</h1>
+        <Form.Group controlId="sign-in-email-address">
+          <Form.Control
+            type="email"
+            size="lg"
+            placeholder="Email"
+            autoComplete="username"
+            className="position-relative"
+          />
+        </Form.Group>
+        <Form.Group controlId="sign-in-password" className="mb-3">
+          <Form.Control
+            type="password"
+            size="lg"
+            placeholder="Contraseña"
+            autoComplete="password"
+            className="position-relative"
+          />
+        </Form.Group>
+        <Form.Group
+          controlId="sign-in-remember-me"
+          className="d-flex justify-content-center mb-4"
+        >
+          <Form.Check
+            label="Remember me"
+            size="lg"
+            placeholder="Contraseña"
+            autoComplete="password"
+            className="position-relative"
+          />
+        </Form.Group>
+        <div className="d-grid">
+          <Button variant="primary" size="lg">
+            Entrar
+          </Button>
+        </div>
+        <p className="mt-5">&copy; 2021-2022</p>
+      </Form>
+    </Container>
+  );
+};
+
+export default Formulario;
